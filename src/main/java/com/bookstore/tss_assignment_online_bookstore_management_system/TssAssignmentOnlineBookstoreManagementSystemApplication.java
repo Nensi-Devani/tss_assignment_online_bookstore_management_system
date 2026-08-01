28c4629335497466bf09c2de7cdd41ee5efbee41
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-01 18:48:14 +0530

    first commit

---
+++ src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/TssAssignmentOnlineBookstoreManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.tss_assignment_online_bookstore_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TssAssignmentOnlineBookstoreManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TssAssignmentOnlineBookstoreManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstore/tss_assignment_online_bookstore_management_system/TssAssignmentOnlineBookstoreManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.tss_assignment_online_bookstore_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TssAssignmentOnlineBookstoreManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
