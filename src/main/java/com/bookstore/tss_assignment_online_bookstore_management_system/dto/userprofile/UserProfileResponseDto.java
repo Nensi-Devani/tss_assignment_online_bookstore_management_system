e484360e12088b1a916fc7f8cad985652457c305
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-03 10:15:27 +0530

    updated response dto

--- src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/dto/userprofile/UserProfileResponseDto.java
+++ src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/dto/userprofile/UserProfileResponseDto.java
@@ -1,4 +1,24 @@
 package com.bookstore.tss_assignment_online_bookstore_management_system.dto.userprofile;
 
+import com.bookstore.tss_assignment_online_bookstore_management_system.enums.Status;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class UserProfileResponseDto {
+
+    private Long userProfileId;
+
+    private Long userId;
+
+    private String phone;
+
+    private String address;
+
+    private LocalDate dateOfBirth;
+
+    private String avatarPath;
+
+    private Status status;
 }
