3e0750a445e4c22adb07944d0f98eef4fb159752
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-03 18:56:42 +0530

    add method

--- src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/repository/OrderItemRepository.java
+++ src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/repository/OrderItemRepository.java
@@ -4,6 +4,9 @@ import com.bookstore.tss_assignment_online_bookstore_management_system.entity.Or
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrderOrderId(Long orderId);
 }
