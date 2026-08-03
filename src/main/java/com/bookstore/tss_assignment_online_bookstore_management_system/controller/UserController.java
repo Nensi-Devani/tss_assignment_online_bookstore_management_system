ac4e1d8a8d415e75948bde6110de3b899b68044f
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-03 23:51:01 +0530

    add user controller

---
+++ src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.bookstore.tss_assignment_online_bookstore_management_system.controller;
+
+import com.bookstore.tss_assignment_online_bookstore_management_system.dto.user.UserRequestDto;
+import com.bookstore.tss_assignment_online_bookstore_management_system.dto.user.UserResponseDto;
+import com.bookstore.tss_assignment_online_bookstore_management_system.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> create(@Valid @RequestBody UserRequestDto requestDto) {
+        return new ResponseEntity<>(userService.create(requestDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> getById(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.getById(userId));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDto>> getAll(Pageable pageable) {
+        return ResponseEntity.ok(userService.getAll(pageable));
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> update(@PathVariable Long userId, @Valid @RequestBody UserRequestDto requestDto) {
+        return ResponseEntity.ok(userService.update(userId, requestDto));
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> delete(@PathVariable Long userId) {
+        userService.delete(userId);
+        return ResponseEntity.noContent().build();
+    }
+}
