382dd7a3c0796b2ceed80896d5b8a8a360723c2e
Nensi Devani <nensi.devani@tssconsultancy.com>
2026-08-03 15:53:44 +0530

    add methods

--- src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/repository/BookRepository.java
+++ src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/repository/BookRepository.java
@@ -4,9 +4,15 @@ import com.bookstore.tss_assignment_online_bookstore_management_system.entity.Bo
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    Optional<Book> findByIsbn(String isbn);
+
     boolean existsByIsbn(String isbn);
 
+    List<Book> findByAuthorsAuthorId(Long authorId);
 }
--- src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/repository/ReviewRepository.java
+++ src/main/java/com/bookstore/tss_assignment_online_bookstore_management_system/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.bookstore.tss_assignment_online_bookstore_management_system.reposito
 
 import com.bookstore.tss_assignment_online_bookstore_management_system.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,12 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     List<Review> findByBookBookId(Long bookId);
 
+    List<Review> findByUserUserId(Long userId);
+
+    @Query("""
+            SELECT AVG(r.rating)
+            FROM Review r
+            WHERE r.book.bookId = :bookId
+            """)
+    Double getAverageRating(Long bookId);
 }
